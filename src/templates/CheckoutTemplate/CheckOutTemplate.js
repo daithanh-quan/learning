5aa153bc90f1243050c490480f4c731965f4b249
Your Name <you@example.com>
2022-01-12 21:55:19 +0700

    fix

--- src/templates/CheckoutTemplate/CheckOutTemplate.js
+++ src/templates/CheckoutTemplate/CheckOutTemplate.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router'
-import { INFO_USER } from '../../Redux/types/UserType'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import { accessToken } from '../../Redux/types/UserType'
 // Hometemplate để dựa vào các component truyền vào ở file app.js để render 
 // tránh việc lặp code nếu chuyển qua nhiều trang mà có phần header và footer
 const CheckoutTemplate = (props) => {
-  if (!localStorage.getItem(INFO_USER)) {
+  if (!localStorage.getItem(accessToken)) {
     <Redirect to="/dangnhap" exact />
   }
   const { Component, ...restProps } = props
